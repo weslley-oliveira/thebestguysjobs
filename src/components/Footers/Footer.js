ad759b080123b1326663216d6fffda4628734dca
Weslley Oliveira <weslleyzera2020@gmail.com>
2022-03-01 12:25:45 +0000

    front-end template deploy

--- src/components/Footers/Footer.js
+++ src/components/Footers/Footer.js
@@ -26,7 +26,7 @@ export default function Footer() {
         <div className="container mx-auto px-4">
           <div className="flex flex-wrap text-center lg:text-left">
             <div className="w-full lg:w-6/12 px-4">
-              <h4 className="text-3xl font-semibold">Let's keep in touch!</h4>
+              <h4 className="text-3xl font-semibold">Keep in touch!</h4>
               <h5 className="text-lg mt-0 mb-2 text-blueGray-600">
                 Find us on any of these platforms, we respond 1-2 business days.
               </h5>
@@ -116,7 +116,7 @@ export default function Footer() {
                         className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
                         href="https://creative-tim.com/terms?ref=nr-footer"
                       >
-                        Terms & Conditions
+                        Terms {'&'} Conditions
                       </a>
                     </li>
                     <li>
